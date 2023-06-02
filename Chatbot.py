6c08514907fed28c94b8de5ab64dae70e0579f33
Gingerdude27 <135005651+Gingerdude27@users.noreply.github.com>
2023-06-02 11:41:00 +0200

    V 0.4
    Beenden des Bots und weiterleiten an Support

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,7 @@
 #v 0.1 = Erster Commit; Begrüßung und erste Abfrage
 #v 0.2 = Abfrage Passwort
-#v 0.3 = Anfordern eines Kundenpasswortes
+#v 0.3 = Anfordern eines Kundenpasswortes als Service
+#v 0.4 = Beenden des Bots und weiterleiten an Support
 
 #Begrüßung des Users
 print("Willkomen beim Chatbot der Gruppe 20")
@@ -33,11 +34,28 @@ while authentification == False:
             exit #soll programm beenden
     else:
         print("Das war falsch")
-#
+
+#Ab hier bbeginnt der Chat Teil
 print("Zum beenden schreiben sie bitte ENDE")
-print("Wie kann ich Ihnen weiterhelfen")
-print("")
 
 #Benutzereingabe
-user_input = input("Ihre Frage: ") #Hier wird der input generiert
-print(user_input) #Ausgabe der Benutzereingabe
+trialcount = 0
+while trialcount < 3: #Hier würd überprüft ob der Benutzer schon mehr als 3 Anläufe braucht
+    print("Wie kann ich Ihnen weiterhelfen")
+    frage = input()
+    if frage != "ENDE":
+        print("Hallo") #Hier Frage Antwort
+    else:
+        trialcount = 2000
+else:
+    if trialcount < 2000:
+        print("Es sieht so aus als ob ich nicht in der Lage bin das Problem zu lösen")
+        print("Möchten Sie von einem Mitarbeiter kontaktiert werden?")
+        print("ja/nein")
+        antwort = input()
+        if antwort == "ja":
+            kontaktdaten = input()
+        else:
+            trialcount - 1
+    else:
+        print("Vielen Dank und bis bald")
