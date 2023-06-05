01359b787ea3b64def2f2ba5525bbaa6acc699cc
Gingerdude27 <richard.scheffler@bechtle.com>
2023-06-05 11:56:11 +0200

    Abfrage Rechnungsnummer

--- Chatbot.py
+++ Chatbot.py
@@ -15,5 +15,26 @@ def ask_yes_no(question):
         else:
             print("Invalid response. Please answer with 'y', 'yes', 'ja', 'n', 'no' or 'nein'.")
 
+def kontakt():
+    print("HHIER DIE KONTAKTMÖGLICHKEITEN ANGEBEN")
 
+#Variable
 
+rechnungsliste = []
+#Begrüßung
+print("Willkomen beim Chatbot")
+print("Um Ihre Identität zu bestimmen, geben Sie bitte Ihre Rechnungsnummer ein")
+rechnungsnummer = ask_question("Sollten Sie keine haben geben Sie bitte 1234 ein")
+rechnn = False
+if rechnungsnummer == "1234":
+    kontakt()
+else:
+    if rechnungsnummer in rechnungsliste:
+        print("Vielen Dank!")
+        rechnn = True
+    else:
+        print("Leider ist die Rechnungsnummer uns nicht bekannt")
+        kontakt()
+        
+if rechnn:
+    
