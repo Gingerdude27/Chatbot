88baf024ce00905dcd1627227f495ff373b334b2
Gingerdude27 <richard.scheffler@bechtle.com>
2023-06-05 11:34:53 +0200

    Def. Fragen

--- Chatbot.py
+++ Chatbot.py
@@ -1,1 +1,19 @@
+#Imports
+#Eigentlich import from util.py
+def ask_question(question):
+    print(question)
+    response = input()
+    return response
+
+def ask_yes_no(question):
+    while True:
+        response = input(question).lower()
+        if response in ['y', 'yes', 'ja']:
+            return True
+        elif response in ['n', 'no', 'nein']:
+            return False
+        else:
+            print("Invalid response. Please answer with 'y', 'yes', 'ja', 'n', 'no' or 'nein'.")
+
+
 
